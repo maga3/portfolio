7e8639e1fa79396cd9b1ce6b99dc464cf3141448
maga3 <72155333+maga3@users.noreply.github.com>
2023-01-03 16:14:21 +0100

    about y carousel bootstrap

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-setTimeout(() => {
-    document.body.removeChild(document.getElementById("gifName"));
-    document.getElementsByTagName("div")[0].style.visibility = "visible";
-}, 9000);
+$(document).ready(function () {
+    if (sessionStorage.getItem('video')=== null) {
+        $('.container').hide();
+        $('<video autoplay id="gifName"><source type="video/webm" src="gif/video-presentacion.webm"></source></video>').insertBefore('.container');
+        setTimeout(() => {
+            $('#gifName').remove();
+            $('.container').show();
+        }, 9000);        
+        sessionStorage.setItem('video','true');
+    }
+});
